d1f6e627618f922acffe067df28a43ee97668827
TJ <tjtradih@gmail.com>
2026-04-20 21:22:21 +0200

    full gam elogic with ev calculation ready

--- src/test.py
+++ src/test.py
@@ -5,10 +5,25 @@ game = bj.Blackjack(finite_deck=True, num_of_decks=1, bets_active=False)
 def strategy():
     return 1 if game.get_player_hand() < 17 else 0
 
-while True:
-    round = game.run(strategy)
-    if round == 1.5: print('--Blackjack--')
-    print('----Win----' if round >= 1 else '----Lose----')
-    print(f"Winrate: {game.get_win_prob * 100()}%")
+choice = 2
+
+#1. semi auto
+if choice == 1:
+    while True:
+        round = game.run(strategy)
+        if round == 1.5: print('--Blackjack--')
+        print('----Win----' if round >= 1 else '----Lose----')
+        print(f"Winrate: {game.get_win_prob() * 100}%")
+        print(f"EV: {game.get_ev()}")
+        if input("Press Enter for Next Round (x to exit): ") == 'x':
+            break
+
+#2. auto
+elif choice == 2:
+    rounds = 100
+
+    for i in range(rounds):
+        round = game.run(strategy)
+    print('--------------------')
+    print(f"Winrate: {game.get_win_prob() * 100}%")
     print(f"EV: {game.get_ev()}")
-    input("Press Enter for Next Round: ")
