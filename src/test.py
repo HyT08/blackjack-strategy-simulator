a94bca002c056f263dd5b216c88b7c676413ed0a
TJ <tjtradih@gmail.com>
2026-04-21 20:07:54 +0200

    simple fixes and added ability to double and turn off game visuals

--- src/test.py
+++ src/test.py
@@ -1,29 +1,26 @@
-import blackjack as bj
+import blackjack
 
-game = bj.Blackjack(finite_deck=True, num_of_decks=1, bets_active=False) 
+game = blackjack.Simulation(finite_deck=True, num_of_decks=1, bets_active=False) 
 
 def strategy():
-    return 1 if game.get_player_hand() < 17 else 0
+    if game.get_player_hand_val() < 17:
+        print('Hit')
+        return 2 if game.get_player_hand_val() == 10 or 11 else 1
+    else: 
+        print('Stand')
+        return 0
 
-choice = 2
 
-#1. semi auto
+choice = 1
+
+#1. semi-auto
 if choice == 1:
     while True:
         round = game.run(strategy)
-        if round == 1.5: print('--Blackjack--')
-        print('----Win----' if round >= 1 else '----Lose----')
-        print(f"Winrate: {game.get_win_prob() * 100}%")
-        print(f"EV: {game.get_ev()}")
-        if input("Press Enter for Next Round (x to exit): ") == 'x':
-            break
+        if input("Press Enter for Next Round (x to exit): ") == 'x': break
 
 #2. auto
 elif choice == 2:
     rounds = 100
-
     for i in range(rounds):
         round = game.run(strategy)
-    print('--------------------')
-    print(f"Winrate: {game.get_win_prob() * 100}%")
-    print(f"EV: {game.get_ev()}")
